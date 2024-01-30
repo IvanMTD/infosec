18336a1fe2ad3f30c26347f156c99881ade8acb2
Ivan Morgan <morgantdarck@gmail.com>
2024-01-30 15:18:30 +0300

    update 30.01.2024 (dd.MM.yyyy) - set admin page for admin authorities

--- src/main/java/net/security/infosec/controllers/SecurityControllerAdvice.java
+++ src/main/java/net/security/infosec/controllers/SecurityControllerAdvice.java
@@ -1,6 +1,8 @@
 package net.security.infosec.controllers;
 
+import lombok.RequiredArgsConstructor;
 import net.security.infosec.models.Implementer;
+import net.security.infosec.services.RoleService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor;
 import org.springframework.security.web.server.csrf.CsrfToken;
@@ -10,7 +12,9 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class SecurityControllerAdvice {
+    private final RoleService roleService;
     @ModelAttribute
     Mono<CsrfToken> csrfToken(ServerWebExchange exchange) {
         Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
@@ -21,6 +25,21 @@ public class SecurityControllerAdvice {
     Mono<String> userName(@AuthenticationPrincipal Implementer implementer){
         if(implementer != null){
             return Mono.just(implementer.getFullName());
+        }else{
+            return Mono.empty();
+        }
+    }
+
+    @ModelAttribute(name = "admin")
+    Mono<Boolean> isAdmin(@AuthenticationPrincipal Implementer implementer){
+        if(implementer != null){
+            return roleService.getRoleById(implementer.getId()).flatMap(role -> {
+                if(role.getAuthorities().size() == 6){
+                    return Mono.just(true);
+                }else{
+                    return Mono.just(false);
+                }
+            });
         }else{
             return Mono.empty();
         }
