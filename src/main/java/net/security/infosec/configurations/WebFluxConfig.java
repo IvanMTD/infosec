0620621d0a32bda909cfcb44bd16a7634f1ee7e7
Ivan Morgan <morgantdarck@gmail.com>
2024-05-20 09:55:43 +0300

    update 20.05.2024 * Увеличение срока работы session cookie

--- src/main/java/net/security/infosec/configurations/WebFluxConfig.java
+++ src/main/java/net/security/infosec/configurations/WebFluxConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.server.session.CookieWebSessionIdResolver;
+import org.springframework.web.server.session.WebSessionIdResolver;
 
+import java.time.Duration;
 import java.util.Map;
 
 @Configuration
@@ -21,5 +24,13 @@ public class WebFluxConfig {
     public HandlerMapping handlerMapping(){
         Map<String, WebFluxWebSocketHandler> handlerMap = Map.of("/web-socket",handler);
         return new SimpleUrlHandlerMapping(handlerMap,1);
+    }
+
+    @Bean
+    public WebSessionIdResolver webSessionIdResolver() {
+        CookieWebSessionIdResolver resolver = new CookieWebSessionIdResolver();
+        resolver.setCookieName("session-info-security");
+        resolver.setCookieMaxAge(Duration.ofHours(12));
+        return resolver;
     }
 }
